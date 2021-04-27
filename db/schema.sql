792e6227e0b93b06627b6525b984423240fb2864
Josh Steward <stewardjosh10@gmail.com>
2021-04-27 21:01:05 +1000

    schema updated

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS restaurants_db;
+CREATE DATABASE restaurants_db;
+
+USE restaurants_db;
+
+CREATE TABLE logs (
+	id INT AUTO_INCREMENT NOT NULL,
+    firstName VARCHAR(250) NOT NULL,
+    location VARCHAR(250) NOT NULL, 
+    menuItems VARCHAR (250) NOT NULL, 
+    thoughts VARCHAR (250) NOT NULL, 
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+		updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+);
